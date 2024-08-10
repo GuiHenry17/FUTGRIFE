e484760f5d5b55f85a0e23b2ecd37089e734cf1f
GuiHenry17 <gui201629@gmail.com>
2024-08-10 17:26:49 -0400

    Criação do Carousel

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,10 +1,29 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import Header from "../components/Header";
+import '../style.css';
+
 
 export default function Home() {
     return (
         <>
-        <h1>Loja do GuizinhoPlays123</h1>
-        <Header/>
+            <h1>Loja do GuizinhoPlays123</h1>
+            <Carousel
+            infiniteLoop
+            useKeyboardArrows
+            autoPlay
+            showArrows={true}
+            showStatus={false}
+            showThumbs={false}
+            dynamicHeight>
+                <div>
+                    <img id='imagem1' src="/imagem1.jpeg" /> 
+                </div>
+                <div>
+                    <img id='imagem2' src="/imagem2.jpeg" />
+                </div>
+            </Carousel>
+            <Header />
         </>
     );
 }
