cd6edaa96bca2fc54f901e926cfe71d1ea391ef2
GuiHenry17 <gui201629@gmail.com>
2024-08-12 23:03:41 -0300

    atualização 2% progresso

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -12,28 +12,34 @@ export default function Home() {
     const [listaProdutos, setProdutos] = useState([
         {
             id: 1,
-            item: "Hambúrguer",
-            imagem: "https://www.assai.com.br/sites/default/files/shutterstock_1806472312.jpg",
-            preco: "R$ 25,99"
+            item: "Camiseta do Palmeiras",
+            imagem: "https://images.tcdn.com.br/img/img_prod/311840/180_camisa_puma_palmeiras_i_2022_96864_1_4bf862f00e34ab550ee800add46408f1.jpg",
+            preco: "R$ 199,90"
         },
         {
             id: 2,
-            item: "Coca-cola 350ml",
-            imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4S15squn95k7qtrVOpMX1MOJGe48y4B7FQ&s",
-            preco: "R$ 5,99"
+            item: "Camiseta do Cruzeiro",
+            imagem: "https://images.tcdn.com.br/img/img_prod/311840/180_camisa_adidas_cruzeiro_i_2022_95157_1_2f63d664584f86b24ac9849935f8dd7d.jpg",
+            preco: "R$ 210,99"
         },
         {
             id: 3,
-            item: "Batatas fritas",
-            imagem: "https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2023/05/batata_frita_destaque_ilustrativo_zona_sul.jpg",
-            preco: "R$ 8,99"
+            item: "Camiseta do Grêmio",
+            imagem: "https://images.tcdn.com.br/img/img_prod/311840/180_camisa_kappa_gremio_prudente_i_2022_106497_1_983e0e59821139fa94c244a3fb53a10b.jpg",
+            preco: "R$ 250,99"
         },
         {
             id: 4,
-            item: "Suco de Frutas",
-            imagem: "https://helenalunardelli.com.br/wp-content/uploads/2013/02/sucos.jpg",
-            preco: "R$ 8,99"
+            item: "Camiseta do Santos",
+            imagem: "https://images.tcdn.com.br/img/img_prod/311840/180_camisa_umbro_santos_ii_2021_juvenil_88980_1_0ef759c545a97055bf0f9736f50d2b15.jpg",
+            preco: "R$ 299,90"
         },
+        {
+            id: 5,
+            item: "Camiseta do Santos",
+            imagem: "https://images.tcdn.com.br/img/img_prod/311840/180_camisa_umbro_santos_ii_2021_juvenil_88980_1_0ef759c545a97055bf0f9736f50d2b15.jpg",
+            preco: "R$ 299,90"
+        }
     ]);
 
     const [listaPedidos, setPedidos] = useState([]);
@@ -77,6 +83,9 @@ export default function Home() {
                 </div>
                 <div className='imagem2'>
                     <img className='imagem2' src="/imagem2.jpeg" />
+                </div>
+                <div className="imagem3">
+                    <img className="imagem3" src='/imagem3.jpeg' />
                 </div>
             </Carousel>
             <br />
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import '../global.css';
+
+export default function Header() {
+    return (
+        <header id='header'>
+            <h1>Loja do GuizinhoPlays123</h1>
+            <nav>
+            <ul>
+                <Link to='/produtos'><li>Produtos</li></Link>
+                <Link to='/ofertas'><li>Ofertas</li></Link>
+            </ul>
+            </nav>
+    
+        </header>
+    )
+}
