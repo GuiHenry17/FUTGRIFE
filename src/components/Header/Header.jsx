15d13e87d7da82d45a0f11ea134d1757136832fc
GuiHenry17 <gui201629@gmail.com>
2024-08-22 20:54:45 -0300

    Possivel Finalização

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header id='header'>
-            <h1>FutGrife</h1>
-            <nav>
+            <img src='/public/imagem4.png' id='imagem4'/>
+            <nav id='navg'>
                 <ul>
                     <Link to='/'><li>Home</li></Link>
                     <Link to='/produtos'><li>Produtos</li></Link>
